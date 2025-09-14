e32c46436fe9c87e649195bb04910e2fe1d661e4
Emre Korkmaz <idyne.ek@gmail.com>
2025-09-15 00:10:16 +0300

    Fix bug

--- src/main/java/com/example/bookhub/mapper/CategoryMapper.java
+++ src/main/java/com/example/bookhub/mapper/CategoryMapper.java
@@ -9,9 +9,9 @@ import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface CategoryMapper {
-    @Mapping(target = "books" , ignore = true)
-    CategoryDTO toDTO(Category category);
 
+    CategoryDTO toDTO(Category category);
+    @Mapping(target = "books" , ignore = true)
     Category toEntity(CategoryDTO categoryDTO);
 
     List<CategoryDTO> toDTOList(List<Category> categoryList);
